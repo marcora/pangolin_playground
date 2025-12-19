c8c2f3139e69d5b934471dc28386793e31b76a03
Edoardo Marcora <edoardo.marcora@gmail.com>
2025-12-18 23:36:13 -0500

    Minor edits

--- sleepstudy.py
+++ sleepstudy.py
@@ -23,25 +23,30 @@ def _():
     import seaborn as sns
     import arviz as az
 
-    from matplotlib import pyplot as plt
     from pangolin import interface as pi
     from pangolin.blackjax import sample, sample_arviz, E
-    return np, pd, pg, pi, plt, sns
+    return np, pd, pg, pi, sns
+
+
+@app.cell(hide_code=True)
+def _(mo):
+    mo.md(r"""
+    ## Data
+    """)
+    return
 
 
 @app.cell
 def _(pd):
-    sleepstudy = pd.read_csv(
-        "https://vincentarelbundock.github.io/Rdatasets/csv/lme4/sleepstudy.csv"
-    )
-
-    sleepstudy.head()
+    sleepstudy = pd.read_csv("https://vincentarelbundock.github.io/Rdatasets/csv/lme4/sleepstudy.csv")
     return (sleepstudy,)
 
 
 @app.cell
 def _(sleepstudy):
     sleepstudy.drop(columns=sleepstudy.columns[0], inplace=True)
+
+    sleepstudy
     return
 
 
@@ -53,9 +58,9 @@ def _(mo):
     $$
     \begin{aligned}
     y_i &\sim \mathcal{N}(\mu_i, \sigma_y) \\
-    \mu_i &= (\beta_0 + a_{s_i}) + (\beta_1 + b_{s_i}) \, x_i \\
-    \beta_0 &\sim \mathcal{N}(0, 1000) \\
-    \beta_1 &\sim \mathcal{N}(0, 1000) \\
+    \mu_i &= (\alpha + a_{s_i}) + (\beta + b_{s_i}) \, x_i \\
+    \alpha &\sim \mathcal{N}(0, 1000) \\
+    \beta &\sim \mathcal{N}(0, 1000) \\
     a_s &\sim \mathcal{N}(0, \sigma_a) \\
     b_s &\sim \mathcal{N}(0, \sigma_b) \\
     \sigma_a &= \exp(\mathcal{N}(0, 2)) \\
@@ -68,50 +73,78 @@ def _(mo):
 
 
 @app.cell
-def _(np, pg, pi, sleepstudy):
-    # data -------------------------------------------------------
+def _(sleepstudy):
     y_obs = sleepstudy["Reaction"].values.astype(float)
-    x     = sleepstudy["Days"].values.astype(float)
-    subj  = sleepstudy["Subject"].values.astype(str)
+    x = sleepstudy["Days"].values.astype(float)
+    subjects = sleepstudy["Subject"].values.astype(str)
+    return subjects, x, y_obs
 
-    _, s  = np.unique(subj, return_inverse=True)
-    N     = len(y_obs)
-    J     = len(np.unique(s))
 
-    # priors -----------------------------------------------------
-    beta0 = pi.normal(0., 1000.)
-    beta1 = pi.normal(0., 1000.)
+@app.cell
+def _(np, subjects, y_obs):
+    _, s = np.unique(subjects, return_inverse=True)
+    N = len(y_obs)
+    J = len(np.unique(s))
+    return J, N, s
 
-    sigma_y = pi.exp(pi.normal(0., 2.))
-    sigma_a = pi.exp(pi.normal(0., 2.))
-    sigma_b = pi.exp(pi.normal(0., 2.))
 
-    a = [pi.normal(0., sigma_a) for _ in range(J)]
-    b = [pi.normal(0., sigma_b) for _ in range(J)]
+@app.cell(hide_code=True)
+def _(mo):
+    mo.md(r"""
+    ## Prior
+    """)
+    return
 
-    # mu linpred ---------------------------------------------
-    mu = [
-        beta0 + a[s[i]] + (beta1 + b[s[i]]) * x[i]
-        for i in range(N)
-    ]
 
-    # likelihood --------------------------------------------------
-    y      = [pi.normal(mu[i], sigma_y) for i in range(N)]
+@app.cell
+def _(J, pi):
+    alpha = pi.normal(0, 1000)
+    beta = pi.normal(0, 1000)
 
-    # posterior predictive ----------------------------------------
+    sigma_y = pi.exp(pi.normal(0, 2))
+    sigma_a = pi.exp(pi.normal(0, 2))
+    sigma_b = pi.exp(pi.normal(0, 2))
+
+    a = [pi.normal(0, sigma_a) for _ in range(J)]
+    b = [pi.normal(0, sigma_b) for _ in range(J)]
+    return a, alpha, b, beta, sigma_y
+
+
+@app.cell(hide_code=True)
+def _(mo):
+    mo.md(r"""
+    ## Likelihood
+    """)
+    return
+
+
+@app.cell
+def _(N, a, alpha, b, beta, pi, s, sigma_y, x):
+    mu = [alpha + a[s[i]] + (beta + b[s[i]]) * x[i] for i in range(N)]
+
+    y = [pi.normal(mu[i], sigma_y) for i in range(N)]
+
     y_pred = [pi.normal(mu[i], sigma_y) for i in range(N)]
+    return y, y_pred
 
-    # inference ---------------------------------------------------
-    params  = y_pred
-    samples = pg.blackjax.sample(params, y, y_obs.tolist())
+
+@app.cell(hide_code=True)
+def _(mo):
+    mo.md(r"""
+    ## Inference
+    """)
+    return
+
+
+@app.cell
+def _(pg, y, y_obs, y_pred):
+    samples = pg.blackjax.sample(y_pred, y, y_obs.tolist())
     return (samples,)
 
 
 @app.cell
-def _(plt, samples, sns):
+def _(samples, sns):
     [sns.kdeplot(sample, legend=False, alpha=0.1) for sample in samples]
-    plt.legend()
-    plt.show()
     return
 
 
